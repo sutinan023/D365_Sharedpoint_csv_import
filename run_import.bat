a39d4a43e4660556fd5a17ad8191a31d23c15f7a
sutinan023 <cs55112.spp@gmail.com>
2026-07-26 15:32:12 +0700

    fix: harden scheduler import wrapper

--- run_import.bat
+++ run_import.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d C:\xampp\htdocs\D365_Sharedpoint_csv_import
-php import\run_pipeline.php >> logs\import.log 2>&1
+if not exist logs mkdir logs
+C:\xampp\php\php.exe import\run_pipeline.php >> logs\import.log 2>&1
